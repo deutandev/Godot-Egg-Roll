46abfbababdc79fe87d4170abc6cfb7dc719eed7
Andika Fathin Azfar <deutandev@pm.me>
2020-11-19 09:15:56 +0700

    Obstacle in same y coordinate with Player

--- src/Main/Main.gd
+++ src/Main/Main.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 
 const OBSTACLE = preload("res://src/Obstacles/Obstacle.tscn")
+# var player_pos = $PlayerNode.position
 
 func _on_ObstacleTimer_timeout():
 	var obs = OBSTACLE.instance()
+	obs.position.y = $PlayerNode.position.y
 	add_child(obs)
