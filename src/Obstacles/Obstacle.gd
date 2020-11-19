e3a20084a14b045de41a429f2647b2c7b0ab0d4c
Andika Fathin Azfar <deutandev@pm.me>
2020-11-19 21:02:48 +0700

    Increase obstacle speed gradually

--- src/Obstacles/Obstacle.gd
+++ src/Obstacles/Obstacle.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
-var obs_speed = 400
+var obs_speed = 350
+
+
 func _process(delta):
 	position.x -= obs_speed * delta
-
+	if Global.current_score % 50 == 0:
+		obs_speed += 1
 
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
