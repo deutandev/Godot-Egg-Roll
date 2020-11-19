486ec85a6a9e7d8ade73b87c961c546028a718fc
Andika Fathin Azfar <deutandev@pm.me>
2020-11-19 14:31:19 +0700

    Reset score _onready()

--- src/Main/Main.gd
+++ src/Main/Main.gd
@@ -5,6 +5,9 @@ const OBSTACLE = preload("res://src/Obstacles/Obstacle.tscn")
 onready var score_label = $Control/CanvasLayer/ScoreLabel
 
 
+func _ready():
+	Global.current_score = 0
+
 func _on_ObstacleTimer_timeout():
 	var obs = OBSTACLE.instance()
 	obs.position.y = $PlayerNode.position.y
