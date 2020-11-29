be7d147fe008411ce5c05cc6e1e2684d35481827
Andika Fathin Azfar <deutandev@pm.me>
2020-11-29 13:00:50 +0700

    Player can shoot bomb to destroy UFO

--- src/Main/Main.gd
+++ src/Main/Main.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 
+const BOMB = preload("res://src/Player/Bomb.tscn")
 onready var score_label = $Control/CanvasLayer/ScoreLabel
-onready var player_pos = $PlayerNode.position.y
+onready var player_pos = $PlayerNode.position
 const OBSTACLE = preload("res://src/Obstacles/Obstacle.tscn")
 const OBS = preload("res://src/Obstacles/Obstacle2.tscn")
 const UFO = preload("res://src/Obstacles/UFO.tscn")
@@ -15,6 +16,9 @@ func _ready():
 	get_random_spawn_time()
 
 
+func _physics_process(delta):
+	shoot()
+
 func get_random_spawn_time():
 	randomize()
 	return rand_range(0.7, 3)
@@ -22,7 +26,7 @@ func get_random_spawn_time():
 
 func _on_ObstacleTimer_timeout():
 	var obs = obs_list[randi() % obs_list.size()].instance()
-	obs.position.y = player_pos
+	obs.position.y = player_pos.y
 	add_child(obs)
 	$ObstacleTimer.wait_time = get_random_spawn_time()
 
@@ -33,3 +37,10 @@ func _on_ScoreTimer_timeout():
 
 func _process(delta):
 	score_label.text = "Score: " + str(Global.current_score)
+
+
+func shoot():
+	if Input.is_action_just_pressed("shoot"):
+		var bomb = BOMB.instance()
+		bomb.position = $PlayerNode/Player.global_position
+		add_child(bomb)
